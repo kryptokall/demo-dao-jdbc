9ccc59f8f442e4fe709b82052425044295a92c5f
kryptokall <lleu.marques29@gmail.com>
2025-01-11 19:16:39 -0300

    findAll implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,5 +19,9 @@ public class Main {
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
         List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
         list.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
     }
 }
