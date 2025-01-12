7834b9f3e50cc65b0e8ca70b21a78ddd7afb2c72
kryptokall <lleu.marques29@gmail.com>
2025-01-12 12:59:35 -0300

    delete implementation in DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,9 +4,13 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.Scanner;
+
 public class Program2 {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -23,6 +27,13 @@ public class Program2 {
         department.setName("Tools");
         departmentDao.update(department);
         System.out.println("Update completed!");
+
+        System.out.println("=== TEST 4: department delete ===");
+        System.out.print("Enter id for deletion: ");
+        departmentDao.deleteById(sc.nextInt());
+        System.out.println("Department deleted!");
+
+        sc.close();
     }
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -74,7 +74,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM department WHERE Id = ? ");
 
+            st.setInt(1, id);
+
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DbException("Nothing changed: Nonexistent department!");
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
